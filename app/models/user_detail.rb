49e2d7fd80e3cb85c60ce904ebfee13088f45228
SirishaGodina <sirishagodina9@gmail.com>
2022-06-23 23:08:17 +0530

    fifth commit

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -2,5 +2,5 @@ class UserDetail < ApplicationRecord
     belongs_to :user 
     validates_uniqueness_of :user_id
     validates :first_name, :last_name, :bank_account_no, presence: true
-    validates :swift_no, numericality: true
+    validates :swift_no, :phone_no, numericality: true
 end
